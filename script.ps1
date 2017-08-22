dc4cd82dace9c7c52400b4854258eb42bcea2544
comchaitu <31233612+comchaitu@users.noreply.github.com>
2017-08-22 11:04:47 +0530

    Add files via upload

--- script.ps1
+++ script.ps1
@@ -7,8 +7,6 @@ $path = 'C:\Users\Training_h1b.06.16\Desktop\CHAITANYA_DEVOPS_P3\Original_xml.xm
 
       
  $node = $xml.DevOpsConfig.Details.DetailsType.Tool
-if $node.value = 'Jenkins'
  $node.Value = $Tool
-fi
    
  $xml.Save($path)
